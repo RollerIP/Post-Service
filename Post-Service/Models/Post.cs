a32e02be3e003806a1266febcd821210696402f8
NHagens <45452499+NHagens@users.noreply.github.com>
2022-05-06 18:15:29 +0200

    Created models

---
+++ Post-Service/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿namespace Post_Service.Models
+{
+    public class Post
+    {
+        public Guid id { get; } 
+        public User user { get; private set; }
+        public string text { get; private set; }
+        public List<string> imageUrls { get; private set; }
+
+        public Post(User user, string text, List<string> imageUrls)
+        {
+            this.id = Guid.NewGuid();
+            this.user = user;
+            this.text = text;
+            this.imageUrls = imageUrls;
+        }
+    }
+}
---
+++ Post-Service/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Post_Service.Models
+{
+    public class User
+    {
+        public Guid id { get; }
+        public string username { get; private set; }
+        public string avatarUrl { get; private set; }
+
+        public User(string username, string avatarUrl)
+        {
+            this.id = Guid.NewGuid();
+            this.username = username;
+            this.avatarUrl = avatarUrl;
+        }
+    }
+}
